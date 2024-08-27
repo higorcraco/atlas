251306e48fe22633bcfb7bdcbef7157a177a0682
higorcraco <higorcraco@gmail.com>
2024-08-26 23:12:11 -0300

    web application created

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import "./config/AxiosInterceptor";
+import Login from "./pages/Login";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return <Login />;
+}
+
+export default App;
