aa38c52116c73b0fc2579880dc679b4222c8d894
higorcraco <higorcraco@gmail.com>
2024-09-01 14:06:24 -0300

    Changed the creator of ExceptionResponse

--- server/src/main/java/br/com/cbtech/atlas/exceptions/ExceptionResponse.java
+++ server/src/main/java/br/com/cbtech/atlas/exceptions/ExceptionResponse.java
@@ -1,16 +1,20 @@
 package br.com.cbtech.atlas.exceptions;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.io.Serializable;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-@AllArgsConstructor
 @Getter
 public class ExceptionResponse implements Serializable {
 
-    private LocalDate timestamp;
+    private LocalDateTime timestamp;
     private String message;
     private String details;
+
+    public ExceptionResponse(String message, String details) {
+        this.message = message;
+        this.details = details;
+        this.timestamp = LocalDateTime.now();
+    }
 }
